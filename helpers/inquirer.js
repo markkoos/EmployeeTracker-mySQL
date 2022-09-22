c9b204fa1bffa2e1df66c541e20d0b87441b2c6c
markkoos <markusimercado@gmail.com>
2022-09-22 13:20:09 -0700

    Finished some of the inquirer prompt

---
+++ helpers/inquirer.js
@@ -0,0 +1,49 @@
+const inquirer = require(`inquirer`);
+
+const startMenu = () => {
+    inquirer.prompt (
+        {
+            type: `list`,
+            message: `What would you like to do?`,
+            name: `startChoice`,
+            choices: [
+                `View All Employees`,
+                `Add Employee`,
+                `Update Employee Role`,
+                `View All Roles`,
+                `Add Role`,
+                `View All Departments`,
+                `Add Department`,
+            ],
+        },
+    )
+    .then((answer) => {
+        switch (answer.startChoice) {
+            case `View All Employees`:
+                console.log(`view all employees chosen`);
+                break;
+            case `Add Employee`:
+                console.log(`add employee chosen`);
+                break;
+            case `Update Employee Role`:
+                console.log(`update employee role chosen`);
+                break;
+            case `View All Roles`:
+                console.log(`view all roles chosen`);
+                break;
+            case `Add Role`:
+                console.log(`add role chosen`);
+                break;                
+            case `View All Departments`:
+                console.log(`view all departments chosen`);
+                break;
+            case `Add Department`:
+                console.log(`Add Department chosen`);
+                break;            
+            
+        };
+        startMenu();
+    })
+}
+
+module.exports = startMenu;
---
+++ index.js
@@ -0,0 +1,17 @@
+// Import mysql2 node module
+const mysql = require(`mysql2`);
+// helper file with inquirer prompts
+const startMenu = require(`./helpers/inquirer`);
+// Import console.table node module
+const cTable = require(`console.table`);
+
+// const db = mysql.createConnection (
+//     {
+//         host: `localhost`,
+//         user: `root`,
+//         password: `Grill12g!`,
+//         database: ``
+//     }
+// );
+
+startMenu()
